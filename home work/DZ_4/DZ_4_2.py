f0b23cbecaad3cf83ac665698522bb49e71b6623
nail14 <40485640+nail14@users.noreply.github.com>
2021-12-09 22:00:31 +0300

    Update_4_2
    Update_4_2

--- home work/DZ_4/DZ_4_2.py
+++ home work/DZ_4/DZ_4_2.py
@@ -1,79 +1,25 @@
 
 from requests import get, utils
-response = get('http://www.cbr.ru/scripts/XML_daily.asp')
-print(type(response))
-print(dir(response))
+from decimal import Decimal
 
-encodings = utils.get_encoding_from_headers(response.headers)
-content = response.content.decode(encoding=encodings)
-print(content)
-# print(response.txt)
+def currency_rates(char_code):
+    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encodings = utils.get_encoding_from_headers(response.headers)
+    server_date = response.headers['Set-Cookie'].split(',')[1]
+    print(f'Дата сервера: {server_date}')
 
-def main(argv):
-    program, *argv = argv
-    result = sum(map(int,argv))
-    print(f'результат: {result}')
+    content = response.content.decode(encoding=encodings)
 
-    return 0
-if __name__ =='__main__':
-    import sys
+    currency_dict = {}
+    for n in content.split('</Value>'):  # Находим код валюты и её значение
+        i = n.split('</CharCode>')[0][-3:]
+        currency_dict[i] = n[-7:] if i.isalpha() else None # Создаем список с валютой и её стоимостью
 
-    print(sys.argv)
+    char_code = char_code.upper()
+    if currency_dict.setdefault(char_code) == None:
+        print('Неверный код валюты')
+    else:                                 #Если ключ есть в списке выдаём значение валюты
+        price = Decimal(currency_dict[char_code].replace(',', '.')).quantize(Decimal('0.01'))
+        print(f'{char_code} = {price} руб.')
 
-    main(sys.argv)
-
-def currency_rates(argv):
-    program, *argv = argv
-    result = sum(map(int, argv))
-    print(f'результат2: {result}')
-
-# print(content.NumCode(float(840)).Value)
-
-currency_rates(input("введите валюту в формате (USD,usd):"))
-for n in content.split('</Value>'):
-    print(n)
-    head = requests.headers["date"]
-    print(head())
-# from requests import request, get, head, post, patch, put, delete, \
-#                      options, utils, session, Session, codes
-#import requests.utils
-#request, get, utils
-# from sys import getsizeof
-
-
-
-
-#
-
-
-
-#encodings = utils.get_encoding_from_headers(response.headers)
-
-
-
-
-
-# print(head str["date"])
-########float
-########<Valute ID="R01235">
-# <NumCode>840</NumCode>
-# <CharCode>USD</CharCode>
-# <Nominal>1</Nominal>
-# <Name>Доллар США</Name>
-# <Value>74,1399</Value>
-# </Valute>
-# def current_rates(*args):
-#     pass
-#http://www.cbr.ru/scripts/XML_daily.asp
-
-# r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-# r.status_code
-# 200
-# r.headers['content-type']
-# 'application/json; charset=utf8'
-# r.encoding
-# 'utf-8'
-# r.text
-# '{"type":"User"...'
-# r.json()
-# print({f'private_gists': 419, 'total_private_repos': 77, ...})
+currency_rates(input('Введите валюту в формате (USD,usd):'))
