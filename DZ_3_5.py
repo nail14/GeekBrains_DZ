42da432488b85ca923baaa04e3a90326359130da
nail14 <40485640+nail14@users.noreply.github.com>
2021-12-09 17:03:18 +0300

    Delete DZ_3_5.py

--- DZ_3_5.py
+++
@@ -1,9 +0,0 @@
-from random import randrange, choice
-nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
-random_idx_1 = randrange(len(nouns))
-adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
-random_idx_2 = randrange(len(adverbs))
-adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-random_idx_3 = randrange(len(adjectives))
-jokes= nouns[random_idx_1], adverbs[random_idx_2], adverbs[random_idx_3]
-print(jokes) #вывод
