dcdc88e59dbbce34979b7df66b864e347fae6c1c
nail14 <40485640+nail14@users.noreply.github.com>
2021-12-11 21:41:39 +0300

    Update DZ_5_1_2
    Update DZ_5_1_2

--- home work/DZ_5/DZ_5_1_2.py
+++ home work/DZ_5/DZ_5_1_2.py
@@ -1,11 +1,17 @@
-set_num_2 = (input('Введите валюту в формате: '))
-set_num = (int(set_num_2))
-print(set_num)
-print(set_num_2)
-def nums_generator(set_num):
-    for num in range(1, set_num + 1, 2):
-        yield num + 2
-num_gen = nums_generator(set_num)
-print(sum(num_gen))
-print(f'>>> next(odd_to_{set_num})')
-print('...StopIteration...')
+###### 5_1
+
+import sys
+
+set_num = (input('Введите число для генерации нечётных чисел: '))
+num_gen = (num for num in range(1, int(set_num) + 1, 2))
+print((num_gen), sys.getsizeof(num_gen),  list(num_gen)) 
+
+
+###### 5_2
+set_num_2 = int(input('Введите число для генерации нечётных чисел (V2): '))
+def nums_generator(set_num_2):
+    for i in range(1, int(set_num_2) + 1 % 2 != 0, 2):
+        # one#set_num
+        yield i*1
+num_gen_2= nums_generator(set_num_2 *1)
+print (type(set_num_2), sys.getsizeof(num_gen), *num_gen)
