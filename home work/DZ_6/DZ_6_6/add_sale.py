2a0854f6edac8b98f6d2f5593ea88effeee991e5
nail14 <40485640+nail14@users.noreply.github.com>
2021-12-15 19:59:47 +0300

    Add files via upload

---
+++ home work/DZ_6/DZ_6_6/add_sale.py
@@ -0,0 +1,16 @@
+
+
+from itertools import zip_longest, islice
+def test_fun():
+    print('Вы поключились к списку счетов, "add_sale"')
+
+
+
+with open('bakery.csv', 'r', encoding='utf-8') as bakery:
+
+    bakery_1=(bakery.read().strip(str(1)).split('\n'))
+    bakery_2 = bakery_1[0:]
+    # print(bakery_2)
+
+if __name__ == '__main__':
+    print('Конец операции')
---
+++ home work/DZ_6/DZ_6_6/show_sales.py
@@ -0,0 +1,16 @@
+# def main(argv):
+#    program, *args = argv
+#    result = sum(map(int, args))
+#    print(f'результат: {result}')
+#
+#    return 0
+#
+#
+# if __name__ == '__main__':
+#    import sys
+#
+#    exit(main(sys.argv))
+
+import add_sale as sales
+sales.test_fun()
+print(list(sales.bakery_2))
