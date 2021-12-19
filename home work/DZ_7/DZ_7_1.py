8ae99a2a96b0b79e9f2bf73a7dd67e5fe6eecc83
nail14 <40485640+nail14@users.noreply.github.com>
2021-12-19 19:40:51 +0300

    Create DZ_7_1.py

---
+++ home work/DZ_7/DZ_7_1.py
@@ -0,0 +1,21 @@
+import os
+import yaml
+
+
+values = ['settings', 'mainapp', 'adminapp', 'authapp']
+keys = 'my_project'
+dict = {keys: values}
+
+
+dir_path = [os.makedirs(os.path.join(keys, i)) for i in values if not os.path.exists(os.path.join(keys, i))]
+
+print(os.path.join(os.path.abspath(os.getcwd())[:0], keys))
+path = os.path.join(os.path.abspath(os.getcwd())[:0], keys)
+for i in values:
+    print(os.path.join(path, i))
+
+with open('config.yaml', 'w') as f:
+    yaml.dump(path, f)
+    for i in values:
+        yaml.dump(os.path.join(path, i), f)
+    pass
