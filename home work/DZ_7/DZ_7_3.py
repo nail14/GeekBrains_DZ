4a40cde66adb42679dca67ea70d9c5e09d8fd8e8
nail14 <40485640+nail14@users.noreply.github.com>
2021-12-20 15:18:37 +0300

    Update DZ_7_3.py

--- home work/DZ_7/DZ_7_3.py
+++ home work/DZ_7/DZ_7_3.py
@@ -1,5 +1,5 @@
 import shutil
 source = "D:\Tutorial\Python\DZ\DZ_7\my_project"
-destination = "D:\Tutorial\Python\DZ\DZ_7\zakavyrka"  #### \templates-но если делать как в методичьке выдает ошибку на \t
+destination = r"D:\Tutorial\Python\DZ\DZ_7\templates" ###разобрался нужно было в сырую строку перевести с помощью r
 new_dir = shutil.move(source, destination)
 print (new_dir)
