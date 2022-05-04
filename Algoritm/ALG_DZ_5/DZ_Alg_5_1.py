63b136dc321182401d5266e36ad02c7dd2f00adc
nail14 <40485640+nail14@users.noreply.github.com>
2022-05-04 16:30:59 +0300

    Update DZ_Alg_5_1.py

--- Algoritm/ALG_DZ_5/DZ_Alg_5_1.py
+++ Algoritm/ALG_DZ_5/DZ_Alg_5_1.py
@@ -6,12 +6,16 @@
 
 
 from collections import namedtuple
+
 Enterprises = namedtuple('Enterprises', 'name quart_1 quart_2 quart_3 quart_4 year')
+
 Count_enteprise = int(input('введите кол-во предприятий: '))
 enterp = [0 for a in range(Count_enteprise)]
+
 total_profit = 0
 
 for i in range(Count_enteprise):
+    
     name = input(f'Введите название {i+1} предприятия: ')
     quarts = [float(k) for k in input('Введите через пробел прибыль за каждый квартал: ').split()]
     year = 0
@@ -21,22 +25,28 @@ for i in range(Count_enteprise):
     enterp[i] = Enterprises(name, *quarts, year)
 
 if Count_enteprise == 1:
+    
     print(f'Предприятие: {enterp[0].name} с годовой прибылью: {enterp[0].year}')
 else:
+    
     average_profit = total_profit / Count_enteprise
     less = []
     more = []
 
     for i in range(Count_enteprise):
+        
         if enterp[i].year < average_profit:
             less.append(enterp[i])
+            
         elif enterp[i].year > average_profit:
             more.append(enterp[i])
 
     print(f'\nСредняя годовая прибыль по всем предприятиям: {average_profit: .2f}')
     print(f'\nПредприятия, чья прибыль меньше: {average_profit: .2f}')
+    
     for t in less:
         print(f'Предприятие {t.name} с прибылью {t.year: .2f}')
     print(f'\nПредприятия, чья прибыль больше: {average_profit: .2f}:')
+    
     for t in more:
         print(f'Предприятие {t.name} с прибылью {t.year: .2f}')
